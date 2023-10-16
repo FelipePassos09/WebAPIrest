1e516a9e4407b98a79940011f0f2fd090fe521ca
FelipePassos09 <felipe.passos.09@gmail.com>
2023-10-16 12:58:15 -0300

    First-Commit

---
+++ Migrations/20231013215210_PopProds.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopProds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,UrlImmage,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Coca-Cola','Refrigerenate de Cola 350ml',5.45,'cocacola.jpg',50,now(),1)");
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,UrlImmage,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Sanduiche de Presunto','Sanduiche frio de presunto e queijo.',6.25,'sndpresunto.jpg',50,now(),2)");
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,UrlImmage,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Pudim de Leite','Pudim tradicional de leite condensado.',8.50,'pdmleite.jpg',50,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Produtos");
+        }
+    }
+}
